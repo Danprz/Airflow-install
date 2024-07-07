c0330e6af4a984b76c8924ff250bcb6bd907d9a9
Daniprz <danperezm93@gmail.com>
2024-07-07 19:34:59 +0200

    Monitoring

---
+++ dags/5.3-orchestration.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime
+
+with DAG(dag_id="orchestration3",
+         description="orchestration with intervals crontab ",  
+         schedule_interval="@monthly",
+         start_date=datetime(2024,1,1),
+         end_date=datetime(2024,6,1)) as dag:
+
+    t1 = EmptyOperator(task_id="task_1")
+
+    t2 = EmptyOperator(task_id="task_2")
+
+    t3 = EmptyOperator(task_id="task_3")
+
+    t4 = EmptyOperator(task_id="task_4")
+    
+    t1 >> t2 >> t3 >> t4
